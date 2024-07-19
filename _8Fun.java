4edd296c2b4c207eea2b752f6a42ed1ca2460fb2
CHANCHALPATIDAR123 <169046773+CHANCHALPATIDAR123@users.noreply.github.com>
2024-07-19 23:43:26 +0530

    Add files via upload

---
+++ _8Fun.java
@@ -0,0 +1,15 @@
+
+class add {
+    public void sum() {
+        System.out.println("hello");
+    }
+}
+
+public class _8Fun {
+    public static void main(String[] args) {
+        // add ad=new add();
+        // add ob;
+        add ap = new add();
+        ap.sum();
+    }
+}
