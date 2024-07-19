58141a11a03a45590034949d23805c20012580da
CHANCHALPATIDAR123 <169046773+CHANCHALPATIDAR123@users.noreply.github.com>
2024-07-19 23:45:04 +0530

    Add files via upload

---
+++ _13Reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class _13Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rem, rev = 0;
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
+        while (num != 0) {
+            rem = num % 10;
+            rev = (rev * 10) + rem;
+            num = num / 10;
+        }
+        System.out.println("revers number:" + rev);
+        sc.close();
+    }
+}
