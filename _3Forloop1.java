f413b92308e304028f22edd3e9e7c6a5b69f5897
CHANCHALPATIDAR123 <169046773+CHANCHALPATIDAR123@users.noreply.github.com>
2024-07-19 23:41:53 +0530

    Add files via upload

---
+++ _3Forloop1.java
@@ -0,0 +1,27 @@
+public class _3Forloop1 {
+    public static void main(String[] args) {
+        /*
+         * for (int i = 1; i <= 10; i++) {
+         * System.out.println("5*" + i + "=" + 5 * i);
+         * }
+         */
+        /*
+         * for (int i = 1; i <= 100; i++) {
+         * if (i % 2 == 0) {
+         * System.out.println("even number=" + i);
+         * }
+         * }
+         * 
+         * for (int i = 1; i <= 100; i++)
+         * if (i % 2 != 0) {
+         * System.out.println("odd number:" + i);
+         * }
+         * for (int i = 1; i <= 50; i++) {
+         * System.out.println(2 * i + 1);
+         * }
+         */
+        for (int i = 0; i <= 50; i++) {
+            System.out.println(2 * i + 1);
+        }
+    }
+}
