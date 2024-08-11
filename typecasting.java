66ad11179b67b6848b56f85549170762675ed028
CHANCHALPATIDAR123 <169046773+CHANCHALPATIDAR123@users.noreply.github.com>
2024-08-11 23:27:35 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,8 @@
+public class typecasting {
+    public static void main(String[] args) {
+        double p = 100.235;
+        int i = (int) p;
+        System.out.println("p=" + p);
+        System.out.println("i=" + i);
+    }
+}
